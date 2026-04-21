02233d21c2f09e5d6cc0a96b420a17bc854f3199
Einstein <93481932+zzzzhub@users.noreply.github.com>
2026-04-21 14:20:42 -0400

    Create strings_tutorial.hpp

---
+++ strings_tutorial.hpp
@@ -0,0 +1,18 @@
+namespace Strings {
+    const uintptr_t FlogDataBank = WindowsRealtimeProtocolEnabled // string use xref
+    const uintptr_t Print = Current identity is %d // string use xref
+    const uintptr_t KTable = Trying to call method on object of type: `%s` with incorrect arguments. // string use xref
+    const uintptr_t LockViolationInstanceCrash =  LockViolationInstanceCrash // string use xref
+    const uintptr_t luaH_dummynode = Attempt to migrate WeakObjectRef across VM boundary // string use xref
+    const uintptr_t luaO_nilobject = Attempt to migrate WeakObjectRef across VM boundary // string use xref
+    const uintptr_t luaT_Eventnames = __call // string use xref
+    const uintptr_t luaT_typenames = userdata // string use xref
+    const uintptr_t RawScheduler = HumanoidParallelManagerTaskQueue // string use xref
+    const uintptr_t TaskScehulderTargetFps = TaskSchedulerTargetFps // string use xref
+    const uintptr_t OpcodeLookupTable = ; R%d: %s%s from %d to %d // string use xref
+    const uintptr_t FireTouchInterest = new overlap in different world // string use xref
+    const uintptr_t GetGlobalState = Script Start // string use xref
+    const uintptr_t GetFFlag = _DataCenterFilter // string use xref
+    const uintptr_t HashTableLookup = Unable to query property {}. It is not scriptable // string use xref
+    const uintptr_t ScriptContextResume = Script execution is not allowed in this context | ScriptResumeBlocked // string use xref
+}
