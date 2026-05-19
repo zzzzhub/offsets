2f1919d23892ed97506fa3fcc17afc5337d37eb2
Einstein <93481932+zzzzhub@users.noreply.github.com>
2026-05-19 11:25:23 -0400

    Update strings_tutorial.hpp

--- strings_tutorial.hpp
+++ strings_tutorial.hpp
@@ -3,7 +3,7 @@
 namespace Strings {
     const uintptr_t FlogDataBank = "WindowsRealtimeProtocolEnabled" // string use xref
     const uintptr_t Print = "Current identity is %d // string use xref"
-    const uintptr_t Print = "print identity" // string use xref or make into func
+    const uintptr_t Print_alternative = "print identity" // string use xref or make into func
     const uintptr_t KTable = "Trying to call method on object of type: `%s` with incorrect arguments." // string use xref
     const uintptr_t LockViolationInstanceCrash =  "LockViolationInstanceCrash" // string use xref
     const uintptr_t luaH_dummynode = "Attempt to migrate WeakObjectRef across VM boundary" // string use xref (check relative location)
@@ -18,4 +18,9 @@ namespace Strings {
     const uintptr_t GetFFlag = "_DataCenterFilter" // string use xref
     const uintptr_t HashTableLookup = "Unable to query property {}. It is not scriptable" // string use xref
     const uintptr_t ScriptContextResume = "Script execution is not allowed in this context | ScriptResumeBlocked" // string use xref
+    const uintptr_t luaL_argerror = "bad argument #%d to '%s' (%s expected, got %s)"  // string use xref
+    const uintptr_t lua_yield = "Attempt to yield across metamethod/C-call boundary"  // string use xref
+    const uintptr_t lua_newstate = "not enough memory"  // string use xref
+    const uintptr_t lua_concat = "attempt to concatenate %s with %s"  // string use xref
+    const uintptr_t lua_pcall = "error in error handling"  // string use xref
 }
