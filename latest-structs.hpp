d08b326a55010559bb06b196e863261ec3d808de
Einstein <93481932+zzzzhub@users.noreply.github.com>
2026-05-19 18:29:25 -0400

    Update latest-structs.hpp

--- latest-structs.hpp
+++ latest-structs.hpp
@@ -1,1 +1,11 @@
-// nothing yet <3
+// version-9377ee10133e4be3
+#pragma once
+#include <cstdint>
+
+#define PROTO_LINEINFO_ENC     VMValue2
+#define PROTO_TYPEINFO_ENC     VMValue1
+#define PROTO_DEBUGINSN_ENC    VMValue3
+#define PROTO_UPVALUES_ENC     VMValue4
+#define PROTO_LOCVARS_ENC      VMValue2
+
+// dumping more soon <3
